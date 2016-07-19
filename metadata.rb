ce943988e0353097858f1d799b5aa0a455d833c4
Jeff Wallace <jeff@zozi.com>
2016-07-19 14:26:28 -0700

    Bump version to 0.0.9

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "dev@zozi.com"
 license "Apache 2.0"
 description "Chef cookbook for installing passenger with rbenv"
 long_description "Please refer to README.md."
-version "0.0.7"
+version "0.0.9"
 
 depends "ruby_rbenv"
 depends "apache2", ">= 2.0.0"
